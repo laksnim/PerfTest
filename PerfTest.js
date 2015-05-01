5978034281e1f770055751217c60181a7cd64df0
laksnim <lakshmidurga.n@gmail.com>
2015-05-01 15:23:55 -0400

    Update PerfTest.js

--- PerfTest.js
+++ PerfTest.js
@@ -15,6 +15,7 @@ function releaseCode() {
         tcp = t.connectEnd - t.connectStart,
         ttfb = t.responseStart - t.navigationStart;
 
+
     $("body").append("<div id='perfBox' style='right:0;padding-left:20px;position: fixed;width:850px;margin: 0;top: 0;z-index: 900;background: rgba(255, 255, 255, 0.95);color: #555;font-size: 20px;max-height: 500px;overflow-y:scroll;resize: both'><table id='myContent' style='width:100%'><thead><tr><th style='padding: 15px;padding-bottom:10px;width: 500px;word-wrap: break-word;'>URL</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Duration</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>DNS</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>TCP</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Waiting</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Content</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Network Duration</th></tr></thead><tbody id='contentBody'></tbody></table></div>");
 
     var perfBox = $('#perfBox');
@@ -24,18 +25,40 @@ function releaseCode() {
     // Resource Timing
     var r = performance.getEntriesByType("resource");
 
+
+    function sortProperties(obj)
+    {
+        // convert object into array
+        var sortable=[];
+        for(var key in obj)
+            if(obj.hasOwnProperty(key))
+                sortable.push([key, obj[key]]); // each item is an array in format [key, value]
+
+        // sort items by value
+        sortable.sort(function(a, b)
+        {
+            var x=a[1].duration,
+                y=b[1].duration;
+            return x>y ? -1 : x<y ? 1 : 0;
+        });
+        return sortable;
+    }
+
+    var sortedResources=sortProperties(r);
+
     for(var i=0;i<=r.length;i++){
-        var dnsR=r[i].domainLookupEnd - r[i].domainLookupStart;
-        var tcpR=r[i].connectEnd - r[i].connectStart;
-        var waitingR=r[i].responseStart - r[i].startTime;
-        var contentR = r[i].responseEnd - r[i].responseStart;
+        var resource= (sortedResources[i])[1];
+        var dnsR=resource.domainLookupEnd - resource.domainLookupStart;
+        var tcpR=resource.connectEnd - resource.connectStart;
+        var waitingR=resource.responseStart - resource.startTime;
+        var contentR = resource.responseEnd - resource.responseStart;
         var networkDuration = dnsR+tcpR+waitingR+contentR;
 
-        var resUrl = r[i].name.split('/');
+        var resUrl = resource.name.split('/');
         var urlLength = resUrl.length;
         var shortenedUrl= resUrl[0]+resUrl[1]+resUrl[2]+"..."+(resUrl[urlLength-1].split('?'))[0];
 
-        $('#contentBody').append("<tr style='border-bottom: solid 1px orange'><td style='color:orange;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+shortenedUrl+"</td><td style='text-align: center'>"+Math.round(r[i].duration)+"</td><td style='text-align: center'>"+Math.round(dnsR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(tcpR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(waitingR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(contentR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(networkDuration).toFixed(2)+"</td></tr>");
+        $('#contentBody').append("<tr style='border-bottom: solid 1px orange'><td style='color:orange;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+shortenedUrl+"</td><td style='text-align: center'>"+Math.round(resource.duration)+"</td><td style='text-align: center'>"+Math.round(dnsR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(tcpR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(waitingR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(contentR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(networkDuration).toFixed(2)+"</td></tr>");
 
     }
 }
