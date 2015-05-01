6a8ff3690efd9b430153b9cc5c4696251b659c15
laksnim <lakshmidurga.n@gmail.com>
2015-05-01 15:55:08 -0400

    Update PerfTest.js

--- PerfTest.js
+++ PerfTest.js
@@ -16,11 +16,11 @@ function releaseCode() {
         ttfb = t.responseStart - t.navigationStart;
 
 
-    $("body").append("<div id='perfBox' style='right:0;padding-left:20px;position: fixed;width:850px;margin: 0;top: 0;z-index: 900;background: rgba(255, 255, 255, 0.95);color: #555;font-size: 20px;max-height: 500px;overflow-y:scroll;resize: both'><table id='myContent' style='width:100%'><thead><tr><th style='padding: 15px;padding-bottom:10px;width: 500px;word-wrap: break-word;'>URL</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Duration</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>DNS</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>TCP</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Waiting</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Content</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Network Duration</th></tr></thead><tbody id='contentBody'></tbody></table></div>");
+    $("body").append("<div id='perfBox' style='right:0;padding-left:20px;position: fixed;width:850px;margin: 0;top: 0;z-index: 900;background: rgba(255, 255, 255, 0.95);color: #555;font-size: 20px;max-height: 500px;overflow-y:scroll;resize: both'><table id='myContent' style='width:100%'><thead><tr><th style='padding: 15px;padding-bottom:10px;width: 500px;word-wrap: break-word;'>Ratio (%)</th><th style='padding: 15px;padding-bottom:10px;width: 500px;word-wrap: break-word;'>URL</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Duration (ms)</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>DNS (ms)</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>TCP (ms)</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Waiting (ms)</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Content (ms)</th><th style='text-align: center;padding: 15px;padding-bottom:10px;'>Network Duration (ms)</th></tr></thead><tbody id='contentBody'></tbody></table></div>");
 
     var perfBox = $('#perfBox');
 
-    perfBox.prepend('<p style="margin-top:20px"><span style="margin-right: 20px">'+'Page Load Time:'+pageloadtime+'</span><span style="margin-right: 20px">'+'DNS:'+dns+'</span><span style="margin-right: 20px">'+'TCP:'+tcp+'</span><span style="margin-right: 20px">'+'TTFB:'+ttfb+'</span></p>');
+    perfBox.prepend('<p style="margin-top:20px"><span style="margin-right: 20px">'+'Page Load Time:'+pageloadtime+'ms'+'</span><span style="margin-right: 20px">'+'DNS:'+dns+'ms'+'</span><span style="margin-right: 20px">'+'TCP:'+tcp+'ms'+'</span><span style="margin-right: 20px">'+'TTFB:'+ttfb+'ms'+'</span></p>');
 
     // Resource Timing
     var r = performance.getEntriesByType("resource");
@@ -45,7 +45,7 @@ function releaseCode() {
     }
 
     var sortedResources=sortProperties(r);
-console.log(sortedResources);
+
     for(var i=0;i<=r.length;i++){
         var resource= (sortedResources[i])[1];
         var dnsR=resource.domainLookupEnd - resource.domainLookupStart;
@@ -58,7 +58,9 @@ console.log(sortedResources);
         var urlLength = resUrl.length;
         var shortenedUrl= resUrl[0]+resUrl[1]+resUrl[2]+"..."+(resUrl[urlLength-1].split('?'))[0];
 
-        $('#contentBody').append("<tr style='border-bottom: solid 1px orange'><td style='color:orange;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+shortenedUrl+"</td><td style='text-align: center'>"+Math.round(resource.duration)+"</td><td style='text-align: center'>"+Math.round(dnsR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(tcpR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(waitingR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(contentR).toFixed(2)+"</td><td style='text-align: center'>"+Math.round(networkDuration).toFixed(2)+"</td></tr>");
+        var ratio= Math.round((resource.duration/pageloadtime)*100);
+
+        $('#contentBody').append("<tr style='border-bottom: solid 1px orange'><td style='color:purple;text-align:center;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+ratio+"</td><td style='color:orange;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+shortenedUrl+"</td><td style='text-align: center'>"+Math.round(resource.duration)+"</td><td style='text-align: center'>"+Math.round(dnsR)+"</td><td style='text-align: center'>"+Math.round(tcpR)+"</td><td style='text-align: center'>"+Math.round(waitingR)+"</td><td style='text-align: center'>"+Math.round(contentR)+"</td><td style='text-align: center'>"+Math.round(networkDuration)+"</td></tr>");
 
     }
 }
