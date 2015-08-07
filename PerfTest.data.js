bd147f7f57fc2b8cbaff0ca0e4e54aa56b121faa
dclangan <dan.langan@gmail.com>
2015-08-07 17:29:56 -0400

    outputting some top offenders data on dashboard

--- PerfTest.data.js
+++ PerfTest.data.js
@@ -5,12 +5,48 @@
     var dns = perfTiming.domainLookupEnd - perfTiming.domainLookupStart;
     var tcp = perfTiming.connectEnd - perfTiming.connectStart;
     var ttfb = perfTiming.responseStart - perfTiming.navigationStart;
+    // Resource Timing
+    var resources = performance.getEntriesByType("resource");
 
+
+
+/*
+    for(var i=0;i<=resources.length;i++){
+        var resource= (sortedResources[i])[1];
+        var dnsR=resource.domainLookupEnd - resource.domainLookupStart;
+        var tcpR=resource.connectEnd - resource.connectStart;
+        var waitingR=resource.responseStart - resource.startTime;
+        var contentR = resource.responseEnd - resource.responseStart;
+        var networkDuration = dnsR+tcpR+waitingR+contentR;
+
+        var resUrl = resource.name.split('/');
+        var urlLength = resUrl.length;
+        var shortenedUrl= resUrl[0]+resUrl[1]+resUrl[2]+"..."+(resUrl[urlLength-1].split('?'))[0];
+
+        var ratio= Math.round((resource.duration/pageloadtime)*100);
+
+        $('#contentBody').append("<tr style='border-bottom: solid 1px orange'><td style='color:purple;text-align:center;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+ratio+"</td><td style='color:orange;margin-top: 11px;max-width: 550px;word-wrap: break-word;'>"+shortenedUrl+"</td><td style='text-align: center'>"+Math.round(resource.duration)+"</td><td style='text-align: center'>"+Math.round(dnsR)+"</td><td style='text-align: center'>"+Math.round(tcpR)+"</td><td style='text-align: center'>"+Math.round(waitingR)+"</td><td style='text-align: center'>"+Math.round(contentR)+"</td><td style='text-align: center'>"+Math.round(networkDuration)+"</td></tr>");
+
+    }
+*/
 
 
     PerfTest.Data = {
         getPageLoadTime: function () {
             return pageloadtime;
+        },
+        getSlowResources: function () {
+            var threshold = pageloadtime / 4;
+            var filteredResources = PerfTest.ArrayUtils.objectFilter(resources, {
+                matchType: "all",
+                criteria: [
+                    {field: "duration", value: threshold, operator: "greaterThan"}
+                ]
+            });
+
+            return PerfTest.ArrayUtils.objectSort(filteredResources, [
+                {field: "duration", direction: "desc", type: "number"}
+            ]);
         },
         getBadProtocolCounts: function () {
 
--- PerfTest.topOffenders.js
+++ PerfTest.topOffenders.js
@@ -15,7 +15,23 @@
         },
         getDashboardView: function (params) {
             var html = [];
-            html.push('<div>top offenders module here</div>');
+            var totalLoadTime = PerfTest.Data.getPageLoadTime();
+            var topOffenders = PerfTest.Data.getSlowResources();
+            var maxItems = Math.min(topOffenders.length, 5);
+            var i;
+            console.log("slow stuff", topOffenders)
+            html.push('<div>');
+            html.push('  <div>Very slow items: ' + topOffenders.length + '</div>');
+            html.push('  <ul>');
+            for (i = 0; i < maxItems; i += 1) {
+                html.push('    <li>');
+                html.push('      <span class="TopOffenders-ratio">' + Math.round((topOffenders[i].duration/totalLoadTime)*100) + '%</span>');
+                html.push('      <span class="TopOffenders-url">' + topOffenders[i].name + '</span>');
+                html.push('    </li>');
+            }
+            html.push('  <ul>');
+
+            html.push('</div>');
             //            params = {
             //                width: 0,
             //                height: 0
