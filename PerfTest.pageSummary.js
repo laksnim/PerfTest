ab87d0dc2bc2d29f900cbb8e4be69ce656eb92a9
laksnim <lakshmidurga.n@gmail.com>
2015-08-12 21:10:59 -0400

    Update PerfTest.pageSummary.js

--- PerfTest.pageSummary.js
+++ PerfTest.pageSummary.js
@@ -13,11 +13,12 @@
             $('<div>page summary detail view hello</div>').appendTo(params.element);
         },
         createDashboardView: function (params) {
+            console.log(params);
             var html = [];
             var totalLoadTime = PerfTest.Data.getPageLoadTime();
 
             html.push('<div>');
-            html.push('  <div>Page load time: ' + totalLoadTime + '</div>');
+            html.push('  <div>Page load time: ' + hello totalLoadTime + '</div>');
             html.push('</div>');
 
             $(html.join('\n')).appendTo(params.element);
